d7bd8f8bb901bb402802d8d7b84bb01caacd12c1
tnp554 <115107221+tnp554@users.noreply.github.com>
2025-05-05 22:03:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.get('/message', (req, res) => {
-  res.json({ message: 'Welcome to the API!' });
+  res.json({ message: 'Fetched From Backend!!!' });
 });
 
 app.post('/submit', (req, res) => {
